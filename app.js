f238c5f9fe8515c310ecd23ef2e71adccb6c90cb
GalisGalisteo <galis.travelling@gmail.com>
2023-10-20 08:38:40 +0200

    ex finished

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 // Recuperamos TODOS los elementos HTML que cumplen con la condición class="button"
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+
+// body.addEventListener("click", (event) => {
+//     if (event.target.id === 'grey') {
+//         body.style.background = 'grey';
+//     } else if (event.target.id === 'white') {
+//         body.style.background = 'white';
+//     } else if (event.target.id === 'blue') {
+//         body.style.background = 'blue';
+//     } else if (event.target.id === 'yellow')
+//         body.style.background = 'yellow';
+// })
+
+body.addEventListener("click", (event) => {
+    body.style.background = event.target.id;
+})
